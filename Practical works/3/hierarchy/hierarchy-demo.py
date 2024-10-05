cc6c2296df5a99ea45f7c99c8797d4c13dc0f2b3
tedeev <tedeev@ursalab.ru>
2024-10-05 18:18:05 +0500

    Prac 3
    Добавил в демо вывод перегруженных методов.

--- Practical works/3/hierarchy/hierarchy-demo.py
+++ Practical works/3/hierarchy/hierarchy-demo.py
@@ -8,8 +8,8 @@ from person import Person
 """
 def print_person() -> None:
     person = Person('Кирилл', 'Давыдов', 35, True)
-    print(f'{person}\n')
-    print(f'{repr(person)}\n')
+    another_person = Person('Жорик', 'Псковский', 23, True)
+    check_overloaded_methods(person, another_person)
 
 
 """
@@ -17,17 +17,17 @@ def print_person() -> None:
 """
 def print_pupil() -> None:
     pupil = Pupil('Петр', 'Воронцов', 13, True, '№25', 7)
-    print(f'{pupil}\n')
-    print(f'{repr(pupil)}\n')
+    another_pupil = Pupil('Лёша', 'Лупин', 15, True, '№25', 9)
+    check_overloaded_methods(pupil, another_pupil)
 
 
 """
 Выводит данные по студенту (наследуется от школьника).
 """
 def print_student() -> None:
-    student = Student('Игорь', 'Тортов', 25, True, 'ПНИПУ', 4, 'АСУ8-13-1м', 'АСУ')
-    print(f'{student}\n')
-    print(f'{repr(student)}\n')
+    student = Student('Игорь', 'Тортов', 35, True, 'ПНИПУ', 4, 'АСУ8-13-1м', 'АСУ')
+    another_student = Student('Олег', 'Попов', 25, True, 'ПНИПУ', 4, 'АСУ2-13-1м', 'АСУ')
+    check_overloaded_methods(student, another_student)
 
 
 """
@@ -35,8 +35,22 @@ def print_student() -> None:
 """
 def print_part_time_student() -> None:
     part_time_student = PartTimeStudent('Сергей', 'Туф', 25, True, 'ПНИПУ', 4, 'АСУ8-13-1мз', 'АСУ', 'ЭТФ-4202-199')
-    print(f'{part_time_student}\n')
-    print(f'{repr(part_time_student)}\n')
+    another_part_time_student = PartTimeStudent('Сергей', 'Туф', 25, True, 'ПНИПУ', 4, 'АСУ8-13-1мз', 'АСУ', 'ЭТФ-4202-199')
+    check_overloaded_methods(part_time_student, another_part_time_student)
+
+
+"""
+Выводит результат проверки перегруженных методов.
+"""
+def check_overloaded_methods(person, another_person) -> None:
+    print(f'str: {person}\n')
+    print(f'repr: {repr(person)}\n')
+    print(f'==: {person == another_person}')
+    print(f'!=: {person != person}')
+    print(f'>: {person > another_person}')
+    print(f'>=: {person >= another_person}')
+    print(f'<=: {person <= another_person}')
+    print(f'<: {person < another_person}\n')
 
 
 if __name__ == '__main__':

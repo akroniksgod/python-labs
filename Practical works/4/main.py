46a84c9cd354184ce58ed30058a130fd55766a1e
Aleksandr Tedeev <73337098+akroniksgod@users.noreply.github.com>
2024-11-16 14:32:46 +0500

    Prac 4
    - Добавил задание 8 из практики;
    - Добавил типы из typing.

--- Practical works/4/main.py
+++ Practical works/4/main.py
@@ -1,5 +1,6 @@
 import json
 from functools import reduce
+from typing import List, Dict, Callable
 
 '''
 Название файла со странами.
@@ -15,9 +16,9 @@ COUNTRIES_FILE_PATH = "./data/" + COUNTRIES_FILE_NAME
 '''
 Возвращает список стран.
 '''
-def get_countries() -> list[str]:
+def get_countries() -> List[str]:
     f = open(COUNTRIES_FILE_PATH, encoding="utf-8")
-    countries: list[str] = json.load(f)
+    countries: List[str] = json.load(f)
     f.close()
     return countries
 
@@ -25,30 +26,30 @@ def get_countries() -> list[str]:
 '''
 Вывод в консоль результат задач с 1 по 6.
 '''
-def built_in_functions_test(countries: list[str]) -> None:
+def built_in_functions_test(countries: List[str]) -> None:
     print(f"1. Исходная коллекция стран: {countries}")
 
-    capitalised_countries: list[str] = list(map(lambda country: str(country).upper(), countries))
+    capitalised_countries: List[str] = list(map(lambda country: str(country).upper(), countries))
     print(f"2. Коллекция стран прописными буквами: {capitalised_countries}")
 
-    no_land_countries: list[str] = list(filter(lambda country: not ("land" in str(country).lower()), countries))
+    no_land_countries: List[str] = list(filter(lambda country: not ("land" in str(country).lower()), countries))
     print(f"3. Коллекция стран без land: {no_land_countries}")
 
-    six_symbols_countries: list[str] = list(filter(lambda country: len(str(country)) == 6, countries))
+    six_symbols_countries: List[str] = list(filter(lambda country: len(str(country)) == 6, countries))
     print(f"4. Коллекция стран с названием из 6 символов: {six_symbols_countries}")
 
-    less_six_symbols_countries: list[str] = list(filter(lambda country: len(str(country)) < 6, countries))
+    less_six_symbols_countries: List[str] = list(filter(lambda country: len(str(country)) < 6, countries))
     print(f"5. Коллекция стран с названием менее 6 символов: {less_six_symbols_countries}")
 
-    no_e_countries: list[str] = list(filter(lambda country: not str(country).lower()[0] == 'e', countries))
+    no_e_countries: List[str] = list(filter(lambda country: not str(country).lower()[0] == 'e', countries))
     print(f"6. Коллекция стран, которые не начинаются с E: {no_e_countries}")
 
 
 '''
 Функция для построения предложения из задания 7.
 '''
-def construct_countries_sentence(countries: list[str]) -> None:
-    allowed_countries: list[str] = [
+def construct_countries_sentence(countries: List[str]) -> None:
+    allowed_countries: List[str] = [
         'Finland',
         'Sweden',
         'Denmark',
@@ -56,7 +57,7 @@ def construct_countries_sentence(countries: list[str]) -> None:
         'Iceland',
     ]
 
-    allowed_countries_dict: dict[str, str] = {
+    allowed_countries_dict: Dict[str, str] = {
         'Finland': 'Финляндия',
         'Sweden': 'Швеция',
         'Denmark': 'Дания',
@@ -64,7 +65,7 @@ def construct_countries_sentence(countries: list[str]) -> None:
         'Iceland': "Исландия",
     }
 
-    european_countries: list[str] = list(filter(lambda country: country in allowed_countries, countries))
+    european_countries: List[str] = list(filter(lambda country: country in allowed_countries, countries))
 
     def countries_join(countries_str: str, country: str) -> str:
         is_last: bool = european_countries[-1] == country
@@ -83,7 +84,48 @@ def construct_countries_sentence(countries: list[str]) -> None:
     print(f"7. {european_countries_str}")
 
 
+'''
+Замена map.
+'''
+def apply_map_to_countries(
+        countries: List[str],
+        map_func: Callable[[str], str]
+) -> List[str]:
+    return [map_func(country) for country in countries]
+
+
+'''
+Замена filter.
+'''
+def apply_filter_to_countries(
+        countries: List[str],
+        filter_func: Callable[[str], bool]
+) -> List[str]:
+    return [country for country in countries if filter_func(country)]
+
+
+'''
+Функция для задания 8.
+'''
+def higher_rank_functions(countries: List[str]) -> None:
+    map_func: Callable[[str], str] = lambda country: str(country).upper()
+    print(f'8.1 {apply_map_to_countries(countries, map_func)}')
+
+    filter_func: Callable[[str], bool] = lambda country: not ("land" in str(country).lower())
+    print(f'8.2 {apply_filter_to_countries(countries, filter_func)}')
+
+    filter_func = lambda country: len(str(country)) == 6
+    print(f'8.3 {apply_filter_to_countries(countries, filter_func)}')
+
+    filter_func = lambda country: len(str(country)) < 6
+    print(f'8.4 {apply_filter_to_countries(countries, filter_func)}')
+
+    filter_func = lambda country: not str(country).lower()[0] == 'e'
+    print(f'8.5 {apply_filter_to_countries(countries, filter_func)}')
+
+
 if __name__ == '__main__':
-    countries: list[str] = get_countries()
+    countries: List[str] = get_countries()
     built_in_functions_test(countries)
     construct_countries_sentence(countries)
+    higher_rank_functions(countries)
